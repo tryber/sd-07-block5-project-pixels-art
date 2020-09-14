4a7b1c520b5ff3996c875c0e874acede77f196dd
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-09-14 18:41:04 -0300

    ajustes code climate

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const blackColor = document.getElementsByClassName("color")[0];    
-const redColor = document.getElementsByClassName("color")[1];
-const blueColor = document.getElementsByClassName("color")[2];
-const greenColor = document.getElementsByClassName("color")[3];
-   
+const blackColor = document.getElementsByClassName('color')[0];
+const redColor = document.getElementsByClassName('color')[1];
+const blueColor = document.getElementsByClassName('color')[2];
+const greenColor = document.getElementsByClassName('color')[3];
+    
 blackColor.style.backgroundColor = 'black';
 redColor.style.backgroundColor = 'red';
-blueColor.style.backgroundColor = 'blue'; 
+blueColor.style.backgroundColor = 'blue';
 greenColor.style.backgroundColor = 'green';
- 
