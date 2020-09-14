7cca9611e1f83c107fe05909f4eca912e81d7d11
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-09-14 16:46:00 -0300

    Paleta com 4 cores

---
+++ script.js
@@ -0,0 +1,10 @@
+const blackColor = document.getElementsByClassName("color")[0];    
+const redColor = document.getElementsByClassName("color")[1];
+const blueColor = document.getElementsByClassName("color")[2];
+const greenColor = document.getElementsByClassName("color")[3];
+   
+blackColor.style.backgroundColor = 'black';
+redColor.style.backgroundColor = 'red';
+blueColor.style.backgroundColor = 'blue'; 
+greenColor.style.backgroundColor = 'green';
+ 
