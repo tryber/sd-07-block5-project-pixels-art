e02924a0e7ab0f13a52528a8ab1e79887aa62c1a
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-09-14 18:58:25 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const blackColor = document.getElementsByClassName('color')[0];
 const redColor = document.getElementsByClassName('color')[1];
 const blueColor = document.getElementsByClassName('color')[2];
-const greenColor = document.getElementsByClassName('color')[3];
-    
+const greenColor = document.getElementsByClassName('color')[3];   
 blackColor.style.backgroundColor = 'black';
 redColor.style.backgroundColor = 'red';
 blueColor.style.backgroundColor = 'blue';
