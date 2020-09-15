29a386fdff5ddd2f19cbc6fc1bf15c16f386ccb5
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-09-14 23:59:28 -0300

    tag selected

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const blackColor = document.getElementsByClassName('color')[0];
-const redColor = document.getElementsByClassName('color')[1];
-const blueColor = document.getElementsByClassName('color')[2];
-const greenColor = document.getElementsByClassName('color')[3];   
-blackColor.style.backgroundColor = 'black';
-redColor.style.backgroundColor = 'red';
-blueColor.style.backgroundColor = 'blue';
-greenColor.style.backgroundColor = 'green';
+window.onload = function () {
+    const blackColor = document.getElementsByClassName('color')[0];
+    const redColor = document.getElementsByClassName('color')[1];
+    const blueColor = document.getElementsByClassName('color')[2];
+    const greenColor = document.getElementsByClassName('color')[3];
+    blackColor.style.backgroundColor = 'black';
+    redColor.style.backgroundColor = 'red';
+    blueColor.style.backgroundColor = 'blue';
+    greenColor.style.backgroundColor = 'green';
+    };
+
