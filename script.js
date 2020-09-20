f1ca1c16062b44575e3611dda6ca3702259491e6
jepaur5 <jepaur5@gmail.com>
2020-09-20 15:29:27 -0300

    requisito 7

---
+++ script.js
@@ -0,0 +1,8 @@
+let colorSelected = document.getElementsByClassName("color");
+
+for(let i = 0; i < 4; i += 1){
+    colorSelected[i].addEventListener("click", function(){
+        document.querySelector(".selected").classList.remove("selected");
+        colorSelected[i].classList.add("selected")
+    });
+}
