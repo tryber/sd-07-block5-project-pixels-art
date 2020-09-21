2a0fb496eb3a0753e6ae95e203454e9e1c34e9a7
jepaur5 <jepaur5@gmail.com>
2020-09-20 23:21:32 -0300

    requisito 8 trocando comillas

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ for(let i = 0; i < 4; i += 1){
     });
 }
 
+
+for(let i = 0; i < pixel.length; i += 1){
+    pixel[i].addEventListener("click", function(){
+        let selected = document.querySelector(".selected")
+        console.log(selected);
+
+        pixel[i].style.backgroundColor = selected.style.backgroundColor
+        console.log(window.getComputedStyle(selected).backgroundColor)
+    })
+}
