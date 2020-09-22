556a159c2d25bfbb995d2db34cbd8b0950d7f7d7
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-09-22 00:53:17 -0300

    css and java script changes

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 window.onload = function () {
-  const blackColor = document.getElementsByClassName('color')[0];
-  const redColor = document.getElementsByClassName('color')[1];
-  const blueColor = document.getElementsByClassName('color')[2];
-  const greenColor = document.getElementsByClassName('color')[3];
-  blackColor.style.backgroundColor = 'black';
-  redColor.style.backgroundColor = 'red';
-  blueColor.style.backgroundColor = 'blue';
-  greenColor.style.backgroundColor = 'green';
+  const itensPaleta = document.querySelectorAll('li');
+  let chosenColor = document.querySelector('.selected');
+  const allQuadros = document.getElementsByTagName('td');
+
+  function switchColors() {
+    for (let index = 0; index < itensPaleta.length; index += 1) {
+      itensPaleta[index].classList.remove('selected');
+    }
+    this.classList.add('selected');
+    chosenColor = document.querySelector('.selected');
+  }
+
+  function paintPixel() {
+    this.className = `pixel ${chosenColor.classList[1]}`;
+  }
+
+  for (let i = 0; i < itensPaleta.length; i += 1) {
+    itensPaleta[i].addEventListener('click', switchColors);
+  }
+
+  for (let i = 0; i < allQuadros.length; i += 1) {
+    allQuadros[i].addEventListener('click', paintPixel);
+  }
 };
