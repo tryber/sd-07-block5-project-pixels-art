132f2e2567d4bb5f2dd9199dff1b7e9a5ad8c11f
Jesus Palli <jepaur5@gmail.com>
2020-09-23 15:20:59 -0300

    tangamandapio

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let pixel = [];
 let linha = [];
 
 let k = 0
-for (let i = 0; i < numberLines; i +=1){
+for (let i = 0; i < numberLines; i +=1) {
     linha[i] = document.createElement("div");
     linha[i].className = "linha"
     pixelBoard.appendChild(linha[i]);
 
-    for(let j = 0; j < numberLines; j += 1 ){
+    for(let j = 0; j < numberLines; j += 1 ) {
       pixel[k] = document.createElement("div");
       pixel[k].className = "pixel"
       linha[i].appendChild(pixel[k])
@@ -20,30 +20,34 @@ for (let i = 0; i < numberLines; i +=1){
 
 
 let color = document.querySelectorAll(".color");
-for (let i = 0; i < color.length; i += 1){
+
+
+for (let i = 0; i < color.length; i += 1) {
     color[i].addEventListener("click", function(event) {
         let selected = document.querySelector(".selected")
         selected.classList.remove('selected');
-        event.target.className += "selected"
+        event.target.className += " selected"
     })
 }
 
 
 
-for(let i = 0; i < pixel.length; i += 1){
-    pixel[i].addEventListener("click", function(){
+for(let i = 0; i < pixel.length; i += 1) {
+    pixel[i].addEventListener("click", function() {
         let selected = document.querySelector(".selected")
+        console.log(selected)
 
+        //pixel[i].style.backgroundColor = window.getComputedStyle(selected).backgroundColor
         pixel[i].style.backgroundColor = window.getComputedStyle(selected).backgroundColor
-        
+        //console.log(window.getComputedStyle(selected).backgroundColor)
     })
 }
 
 
 let buttonClear = document.getElementById("clear-board");
 
-buttonClear.addEventListener("click", function(){
-    for(let i = 0; i < pixel.length; i += 1){
+buttonClear.addEventListener("click", function() {
+    for(let i = 0; i < pixel.length; i += 1) {
         pixel[i].style.backgroundColor = "white";
     }
 })
